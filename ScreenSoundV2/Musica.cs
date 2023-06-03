a54b485d857b4b6791d3b6da91d1c719b19b2fb7
endrewmota <endrew.desousa@gmail.com>
2023-06-03 11:40:21 -0400

    refactoring the property with get; set; methods

--- ScreenSoundV2/Musica.cs
+++ ScreenSoundV2/Musica.cs
@@ -2,17 +2,18 @@
 
 class Musica
 {
-    public string nome;
-    public string artistas;
-    public int duracao;
-    public bool disponivel;
+    public string None { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+    public string DescricaoResumida => $"A música {None} pertence à banda {Artista}";
 
     public void ExibirFichaTecnica()
     {
-        Console.WriteLine($"Nome: {nome}");
-        Console.WriteLine($"Artista: {artistas}");
-        Console.WriteLine($"Duração: {duracao}");
-        if (disponivel)
+        Console.WriteLine($"Nome: {None}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duração: {Duracao}");
+        if (Disponivel)
         {
             Console.WriteLine("Disponível no plano.");
         }
